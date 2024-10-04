96d0c880493007ea1d0d5079fe85d915fced0f98
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-10-04 12:12:21 +0530

    change db to postgres

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -137,13 +137,16 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
